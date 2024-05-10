e1fbe88217649ee821832327755eb27abd1f385b
kamrul-i <komrul45@gmail.com>
2024-05-10 17:23:14 +0200

    Assignment operator

--- src/arithmetic_operator/Arithmetic_operator.java
+++ src/arithmetic_operator/Arithmetic_operator.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Arithmetic_operator {
 	public static void main(String[] args) {
 		
+
+		
 		try (Scanner input = new Scanner(System.in)) {
-			int num1, num2, result;
+			int num1, num2;
 			
 			System.out.print("Enter first number : ");
 			num1 = input.nextInt();
@@ -18,6 +20,9 @@ public class Arithmetic_operator {
 			double result1 = (double) num1 / num2;
 			System.out.printf("Division : %.2f", result1);
 			System.out.printf("\n");
+			
+			//arithmetic operator মানে হলো (যোগ, বিয়োগ, গুণ, ভাগ, ভাগশেষ)।।
+			//arithmetic operator চিহ্ন ( + - * / % ) ৫টি।।
 			
 		}
 	}
---
+++ src/assignment_operator/Assignment_Operator.java
@@ -0,0 +1,27 @@
+package assignment_operator;
+
+public class Assignment_Operator {
+	public static void main(String[] args) {
+		
+        int x = 3;
+        int y = 2;
+        
+        x+=y; // x + y = 5
+        System.out.println("x = "+x);
+        
+        x-=y;  //x-y=3
+        System.out.println("x = "+x);
+        
+        x*=y;  //x*y=6
+        System.out.println("x = "+x);
+        
+        x/=y;  //x/y=3
+        System.out.println("x = "+x);
+        
+        x%=y;  //x%y=1
+        System.out.println("x = "+x);
+
+		//assignment operator চিহ্ন ( =, +=, -=, *=, /=, %= ) ৬টি।।
+	}
+
+}
