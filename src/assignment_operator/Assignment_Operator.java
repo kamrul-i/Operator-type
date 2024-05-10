e7a4b831609a88d72ce792cec202507eaefce5bf
kamrul-i <komrul45@gmail.com>
2024-05-10 17:32:25 +0200

    Area of triangle.

--- src/assignment_operator/Assignment_Operator.java
+++ src/assignment_operator/Assignment_Operator.java
@@ -1,26 +1,25 @@
 package assignment_operator;
 
+import java.util.Scanner;
+
 public class Assignment_Operator {
 	public static void main(String[] args) {
 		
-        int x = 3;
-        int y = 2;
-        
-        x+=y; // x + y = 5
-        System.out.println("x = "+x);
-        
-        x-=y;  //x-y=3
-        System.out.println("x = "+x);
-        
-        x*=y;  //x*y=6
-        System.out.println("x = "+x);
-        
-        x/=y;  //x/y=3
-        System.out.println("x = "+x);
-        
-        x%=y;  //x%y=1
-        System.out.println("x = "+x);
-
+		//--------------------------------------------------------
+        try (Scanner input = new Scanner (System.in)) {
+			double base, height, area;
+			
+			System.out.print("Enter base : ");
+			base = input.nextDouble();
+			
+			System.out.print("Enter height : ");
+			height = input.nextDouble();
+			
+			area = 0.5 * base * height;
+			
+			System.out.println("Area of triangle : "+area);
+		}
+        //--------------------------------------------------------
 		//assignment operator চিহ্ন ( =, +=, -=, *=, /=, %= ) ৬টি।।
 	}
 
