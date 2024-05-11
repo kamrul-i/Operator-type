f614547281ebae1baa616f83428f1bcecffc2c33
kamrul-i <komrul45@gmail.com>
2024-05-11 15:18:51 +0200

    Unary plus.

--- src/unary_operator/Unary_Demo.java
+++ src/unary_operator/Unary_Demo.java
@@ -3,62 +3,36 @@ package unary_operator;
 public class Unary_Demo {
 	public static void main(String[] args) {
 		
+		 
 		
-		
+		//-------------------------------------------
 		int x = 10;
         int result;
         
-        result = +x;
+        result = +x;    //unary plus যেকোন result কে positive হিসেবে দেখায়।।
         System.out.println("Result : "+result);
         
-        result = -x;
+        result = -x;     //unary minus যেকোন result কে negative হিসেবে দেখায়।।
         System.out.println("Result : "+result);
+        //-------------------------------------------
+
+
+        
+        //-------------------------------------------
+		int y = -10;
+        int result1;
+        
+        result1 = +y;    //unary plus যেকোন result কে positive হিসেবে দেখায়।।
+        System.out.println("Result : "+result1);
+        
+        result1 = -y;     //unary minus যেকোন result কে negative হিসেবে দেখায়।।
+        System.out.println("Result : "+result1);
+        //--------------------------------------------
 
 
         
         
 
-        int y = -10;
-        
-        result = +y;
-        System.out.println("Result : "+result);
-        
-        result = -y;
-        System.out.println("Result : "+result);
-        
-        
-        
-        
-        
-        //Increment Demo..................................
-        y = ++x;  //x=11
-        System.out.println("y = "+y);
-        
-        y = x;   //x=11
-        System.out.println("y = "+y);
-               
-        y = x++;  //x=11
-        System.out.println("y = "+y);
-        
-        y = x;    //x= 12
-        System.out.println("y = "+y);
-        
-        
-        
- 
-        
-        //Decrement Demo......................................
-        y = --x;  //x=11
-        System.out.println("y = "+y);
-        
-        y = x;   //x=11
-        System.out.println("y = "+y);
-        
-        y = x--;  //x=11
-        System.out.println("y = "+y);
-        
-        y = x;    //x= 10
-        System.out.println("y = "+y);
 	}
 
 }
