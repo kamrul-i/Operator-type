4b0267f35337a39440f7e590ff05d3b6ddc17047
kamrul-i <komrul45@gmail.com>
2024-05-11 15:22:53 +0200

    Unary minus.

--- src/unary_operator/Unary_Demo.java
+++ src/unary_operator/Unary_Demo.java
@@ -2,37 +2,18 @@ package unary_operator;
 
 public class Unary_Demo {
 	public static void main(String[] args) {
-		
-		 
-		
-		//-------------------------------------------
-		int x = 10;
-        int result;
-        
-        result = +x;    //unary plus যেকোন result কে positive হিসেবে দেখায়।।
-        System.out.println("Result : "+result);
-        
-        result = -x;     //unary minus যেকোন result কে negative হিসেবে দেখায়।।
-        System.out.println("Result : "+result);
-        //-------------------------------------------
-
 
         
         //-------------------------------------------
 		int y = -10;
-        int result1;
+        int result;
         
-        result1 = +y;    //unary plus যেকোন result কে positive হিসেবে দেখায়।।
-        System.out.println("Result : "+result1);
+        result = +y;    //unary plus যেকোন result কে positive হিসেবে দেখায়।।
+        System.out.println("Result : "+result);
         
-        result1 = -y;     //unary minus যেকোন result কে negative হিসেবে দেখায়।।
-        System.out.println("Result : "+result1);
+        result = -y;     //unary minus যেকোন result কে negative হিসেবে দেখায়।।
+        System.out.println("Result : "+result);
         //--------------------------------------------
-
-
-        
-        
-
 	}
 
 }
